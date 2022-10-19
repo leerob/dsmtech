5ec65a92957bd9f97686dab9d4d3765fd7385826
Lee Robinson <lrobinson2011@gmail.com>
2022-10-18 20:07:26 -0500

    Update to latest Next.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Layout from '../components/Layout';
 import { COMPANIES } from '../companies';
 
 const formatFilePath = (name) => name.replace(/ /g, '_').toLowerCase();
@@ -147,45 +146,43 @@ const TableRow = (company) => {
   );
 };
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <Layout>
-      <div className="flex flex-col px-8 mb-8">
-        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead>
-                  <tr>
-                    <th className="px-6 py-3 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
-                      Name
-                    </th>
-                    <th className="px-6 py-3 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
-                      Description
-                    </th>
-                    <th className="px-6 py-3 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
-                      Type
-                    </th>
-                    <th className="px-6 py-3 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
-                      Social
-                    </th>
-                    <th className="px-6 py-3 bg-gray-100"></th>
-                  </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                  {COMPANIES.sort((a, b) => {
-                    if (a.name < b.name) {
-                      return -1;
-                    }
-                  }).map((company) => (
-                    <TableRow key={company.name} {...company} />
-                  ))}
-                </tbody>
-              </table>
-            </div>
+    <div className="flex flex-col px-8 mb-8">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead>
+                <tr>
+                  <th className="px-6 py-3 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
+                    Name
+                  </th>
+                  <th className="px-6 py-3 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
+                    Description
+                  </th>
+                  <th className="px-6 py-3 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
+                    Type
+                  </th>
+                  <th className="px-6 py-3 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
+                    Social
+                  </th>
+                  <th className="px-6 py-3 bg-gray-100"></th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {COMPANIES.sort((a, b) => {
+                  if (a.name < b.name) {
+                    return -1;
+                  }
+                }).map((company) => (
+                  <TableRow key={company.name} {...company} />
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-    </Layout>
+    </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx}'],
   plugins: [require('@tailwindcss/typography')]
 };
