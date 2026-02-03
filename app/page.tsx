c2f60e30a7049d0e6e9d629169340c1253598b9c
Hamberfim <adhamlingithub@gmail.com>
2026-02-03 14:41:10 -0600

    line 150 added ARIA landmark, role="main"

--- app/page.tsx
+++ app/page.tsx
@@ -147,7 +147,7 @@ export default function HomePage() {
     <div className="flex flex-col px-8 mb-8">
       <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg" role="main">
             <table className="min-w-full divide-y divide-gray-200">
               <thead>
                 <tr>
